bddaffc86bedc0e51942bf941e594925074d8c9e
Adadezer <adadezer@gmail.com>
2022-08-23 14:50:15 -0300

    feat: :art: função solveMeFirst
    função em JavaScript que soma dois números inteiros. (exercicio encontrado no Hacker Rank: https://www.hackerrank.com/)

---
+++ 04-solveMeFirst-funcao.js
@@ -0,0 +1,7 @@
+// Neste exercício vamos aprender como criar uma função em JavaScript que soma dois números inteiros. (exercicio encontrado no Hacker Rank: https://www.hackerrank.com/)
+
+function solveMeFirst(a, b) {
+  return a + b;
+}
+
+console.log(solveMeFirst(7, 5))
